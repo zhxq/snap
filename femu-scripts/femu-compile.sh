c93a26762a70cb95e79269a95fe58032f124c1aa
zhxq <zhxq@zhxqweb.com>
2022-02-01 12:06:10 +0800

    Bump to QEMU 6.0.0

--- femu-scripts/femu-compile.sh
+++ femu-scripts/femu-compile.sh
@@ -4,7 +4,7 @@ NRCPUS="$(cat /proc/cpuinfo | grep "vendor_id" | wc -l)"
 
 make clean
 # --disable-werror --extra-cflags=-w
-../configure --enable-kvm --target-list=x86_64-softmmu --disable-git-update
+../configure --enable-kvm --target-list=x86_64-softmmu --with-git-submodules=validate
 make -j $NRCPUS
 
 echo ""
